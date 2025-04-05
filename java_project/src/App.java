c0109c0d5f3f62b8d170ac90381db0c4bcd54427
Omar Zandag <107774932+omarzandag@users.noreply.github.com>
2025-04-05 22:13:34 +0000

    Update App.java

--- java_project/src/App.java
+++ java_project/src/App.java
@@ -3,8 +3,7 @@ import java.util.function.Function;
 public class App {
     public static void main(String[] args) throws Exception {
         generated generated = new generated();
-        for(int i = 0 ; i<5 ; i++){
+            System.out.println("The password Generated is : \n");
             System.out.println(generated.generatedPass() + "\n");
-        }
     }
 }
